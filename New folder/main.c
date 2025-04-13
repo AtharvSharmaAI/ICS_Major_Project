ec15ea9b77788f10142e3d8bbf2e112e913d2487
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2025-04-14 02:11:02 +0530

    Update main.c

--- New folder/main.c
+++ New folder/main.c
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -14,7 +13,7 @@ void typewriter_print(char *str, unsigned int milliseconds, int colour_code)
     printf("\x1b[38;5;%d;1m", colour_code);
     putchar(*str);
     fflush(stdout);
-    //sleep(milliseconds * 700);
+    // usleep(milliseconds * 700);
     printf("\x1b[0m");
   }
 }
@@ -26,40 +25,9 @@ int main(){
     typewriter_print("Main Menu\n",70,197);
 
     //get the train file
-    typewriter_print("Please select your Dataset file: ",60,208);
+    typewriter_print("Please enter your training file name: ",60,208);
     char filename[200];
-    char test_filename[200];
-    int file;
-    typewriter_print("Please select the file number: \n",60,197);
-    typewriter_print("1. Breast Cancer DataSet\n",60,208);
-    typewriter_print("2. Iris Dataset\n",60,208);
-    typewriter_print("3. Scaled Data\n",60,208);
-    typewriter_print("4. Tvmarketing Dataset\n",60,208);
-    scanf("%d",file);
-    switch(file){
-        case 1:
-        strcpy(filename,"Breast_cancer.csv");
-        strcpy(test_filename,"test_breast.csv");
-        break;
-
-        case 2:
-        strcpy(filename,"iris_flower_dataset.csv");
-        strcpy(test_filename,"test_iris.csv");
-        //filename[0] = "iris_flower_dataset.csv";
-        //test_filename[0]= "test_iris.csv";
-        break;
-
-        case 3:
-        strcpy(filename,"scaled_data.csv");
-        strcpy(test_filename,"test_scaled_data.csv");
-        break;
-
-        case 4:
-        strcpy(filename,"tvmarketing.csv");
-        strcpy(test_filename,"test_tvmarketing.csv");
-        break;
-
-    }
+    scanf("%s",filename);
     FILE* fptr= fopen(filename,"r");
 
     //check if file can be loaded or not
@@ -69,9 +37,9 @@ int main(){
     fclose(fptr);
 
     // get the test file
-    //typewriter_print("Please enter your test file name: ",60,208);
-    
-    //scanf("%s",test_filename);
+    typewriter_print("Please enter your test file name: ",60,208);
+    char test_filename[200];
+    scanf("%s",test_filename);
     FILE* fptr_= fopen(filename,"r");
     if(fptr_==NULL){
         typewriter_print("Test file can't be opened\n",50,197);
@@ -128,8 +96,11 @@ int main(){
     case 4:
         KNN(filename,test_filename);
         break;
-
+    
     case 5:
+        softmax_regression(filename,test_filename);
+        break;
+    case 6:
         exit(0);
 
     default:
@@ -140,3 +111,4 @@ int main(){
 
     return 0;
 }
+
